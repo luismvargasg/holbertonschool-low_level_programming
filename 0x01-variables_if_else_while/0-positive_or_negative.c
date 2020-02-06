c7a261b021e492ae47a0e0225d44273eb0af94f2
luismvargasg <1610@holbertonschool.com>
2020-02-06 19:04:08 +0000

    adding task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,11 +17,11 @@ int main(void)
 	{
 		printf("%d is postive\n", n);
 	}
-	if (n < 0)
+	else if (n < 0)
 	{
 		printf("%d is negative\n", n);
 	}
-	if (n == 0)
+	else
 	{
 		printf("%d is zero\n", n);
 	}

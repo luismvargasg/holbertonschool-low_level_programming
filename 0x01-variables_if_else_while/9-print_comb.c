328897a700c7dbbbefdb8a794842b5f54efdae4e
luismvargasg <1610@holbertonschool.com>
2020-02-07 04:27:00 +0000

    adding task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,18 +7,15 @@
  */
 int main(void)
 {
-	int n = 0;
+	int n;
 
-	while (n < 10)
+	for (n = 0; n < 10; n++)
 	{
 		putchar(n + '0');
 		if (n < 9)
-		{
 			putchar(',');
 			putchar(' ');
 		}
-		n++;
-	}
-	putchar('\n');
-	return (0);
+		putchar('\n');
+		return (0);
 }

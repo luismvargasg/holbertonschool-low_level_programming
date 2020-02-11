bd5092716b81e370de93939910677f320a5b6771
luismvargasg <1610@holbertonschool.com>
2020-02-11 22:05:38 +0000

    adding task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: n is number
+ * Return: Always 0.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar ('+');
+		return (1);
+		_putchar ('\n');
+	}
+
+	else if (n == 0)
+	{
+		_putchar ('0');
+		return (0);
+		_putchar ('\n');
+	}
+
+	else
+	{
+		_putchar ('-');
+		return (-1);
+		_putchar ('\n');
+	}
+}

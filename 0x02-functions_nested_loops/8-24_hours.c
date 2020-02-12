ebfcac955bd4b334eaeb2e5884d34218c4bd9448
luismvargasg <1610@holbertonschool.com>
2020-02-12 05:09:21 +0000

    adding task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ *
+ * Return: Always 0.
+ */
+
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 00; h < 24; h++)
+	{
+		for (m = 00; m < 60; m++)
+			{
+				_putchar (h / 10 + '0');
+				_putchar (h % 10 + '0');
+				_putchar (':');
+				_putchar (m / 10 + '0');
+				_putchar (m % 10 + '0');
+				_putchar ('\n');
+			}
+	}
+}

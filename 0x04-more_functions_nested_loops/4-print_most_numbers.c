04d9560230bb42d2c6d9ade752eb6ec5d918d56b
luismvargasg <1610@holbertonschool.com>
2020-02-13 16:03:55 +0000

    adding task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9 but not 2 and 4
+ *
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+	int c = '0';
+
+	while (c <= '9')
+	{
+		if (c != '2' && c != '4')
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}

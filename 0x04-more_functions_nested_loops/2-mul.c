ae9ded4bc38207857e1e126884e483efd3d7dd42
luismvargasg <1610@holbertonschool.com>
2020-02-13 15:42:12 +0000

    adding task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two integers.
+ *
+ * @a: number a
+ * @b: number b
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = (a * b);
+	return (c);
+}

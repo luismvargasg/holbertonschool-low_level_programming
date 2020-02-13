ca3fb2f0a1a1e73ebec9ea195e535ff20d3099ef
luismvargasg <1610@holbertonschool.com>
2020-02-13 20:03:42 +0000

    adding task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,7 @@
 
 /**
  * print_line - draws a straight line in the terminal.
- *
+ * @n: number of loops
  * Return: Always 0.
  */
 
@@ -11,7 +11,7 @@ void print_line(int n)
 	int x;
 
 	x = n;
-	for (n=0; n < x; n++)
+	for (n = 0; n < x; n++)
 	{
 		_putchar ('_');
 	}

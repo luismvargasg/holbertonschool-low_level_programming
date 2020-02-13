76d16f23a99affc4502dd2a2183e656a6c3b9a0c
luismvargasg <1610@holbertonschool.com>
2020-02-13 23:59:40 +0000

    adding task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,8 +16,10 @@ int main(void)
 			printf("FizzBuzz ");
 		else if (a % 3 == 0)
 			printf("Fizz ");
-		else if (a % 5 == 0)
+		else if ((a % 5 == 0) && a != 100)
 			printf("Buzz ");
+		else if (a % 5 == 0)
+			printf("Buzz");
 		else
 			printf("%d ", a);
 		a++;

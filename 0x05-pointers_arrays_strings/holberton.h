fa83eb958f132a44ac12db7404b17713ea4b321f
luismvargasg <1610@holbertonschool.com>
2020-02-18 14:55:45 +0000

    adding task 0

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,13 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif /* _HOLBERTON_H_ */

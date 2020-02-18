e37b69bfc0d2774767159411c34e4bc4df3d1b72
luismvargasg <1610@holbertonschool.com>
2020-02-18 20:36:07 +0000

    adding task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+
+#include "holberton.h"
+
+/**
+ * print_rev - prints a string, in reverse.
+ * @s: given string
+ */
+
+void print_rev(char *s)
+{
+	int lenght = 0;
+
+	while (s[lenght] != '\0')
+	{
+		lenght++;
+	}
+	for (lenght = lenght - 1; lenght >= 0; lenght--)
+	{
+ 		_putchar (s[lenght]);
+	}
+	_putchar ('\n');
+}

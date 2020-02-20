604f90282bf4e6ce56b1f63195277262e0b4f2df
luismvargasg <1610@holbertonschool.com>
2020-02-20 23:21:29 +0000

    adding task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - Function that reverse an array.
+ * @a: Given array
+ * @n: number of elements of the array
+ * Return: int
+ */
+
+void reverse_array(int *a, int n)
+{
+	int temp = 0;
+	int i = 0;
+
+	while (a[i] < a[n])
+	{
+		temp = a[i];
+		a[i] = a[n - 1];
+		a[n - 1] = temp;
+		i++;
+		n--;
+	}
+}

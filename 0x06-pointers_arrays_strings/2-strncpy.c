185fa5e80a5308828c2886159fc55a032952bd0e
luismvargasg <1610@holbertonschool.com>
2020-02-20 21:17:20 +0000

    adding task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - function that copies a string.
+ * @dest: destination
+ * @src: source
+ * @n: given number
+ * Return: pointer
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int ia = 0;
+	int ib = 0;
+
+	while (ib < n && src[ib] != '\0')
+	{
+		dest[ia++] = src[ib++];
+	}
+	if (ib < n)
+	dest[ia] = '\0';
+
+	return (dest);
+}

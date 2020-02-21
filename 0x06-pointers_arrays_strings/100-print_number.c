fb9822def40604882284d221f4ed5360b209326c
luismvargasg <1610@holbertonschool.com>
2020-02-21 04:07:15 +0000

    adding task 100

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_number - function that prints an integer.
+ * @n: given number.
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar ('-');
+		n = -n;
+	}
+
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar (n % 10 + '0');
+}

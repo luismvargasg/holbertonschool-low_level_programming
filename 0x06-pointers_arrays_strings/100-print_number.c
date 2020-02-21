12260e5fff792e79a954f5d9ae5ea4ae185b064d
luismvargasg <1610@holbertonschool.com>
2020-02-21 04:41:35 +0000

    adding task 100

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -21,11 +21,11 @@ void print_number(int n)
 		n = -n;
 	}
 
-	if (n / 10 != 0)
+	if (n / 10)
 		print_number(n / 10);
-
-	_putchar (n % 10 + '0');
 
 	if (add == 1)
 		_putchar ('8');
+	else
+		_putchar (n % 10 + '0');
 }

ccbd21e44efefd5337def1a4b9ee522fe6bae644
luismvargasg <1610@holbertonschool.com>
2020-02-21 04:44:08 +0000

    adding task 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,44 +1,31 @@
 #include "holberton.h"
 
 /**
- * print_number - prints number.
- * @n : number
- * Return: Always 0.
+ * print_number - function that prints an integer.
+ * @n: given number.
  */
 
 void print_number(int n)
 {
+	int add = 0;
+
+	if (n == -2147483648)
+	{
+		n = n + 1;
+		add = 1;
+	}
+
 	if (n < 0)
 	{
-		_putchar (45);
-		_putchar (-n / 10 + '0');
-		_putchar (-n % 10 + '0');
+		_putchar ('-');
+		n = -n;
 	}
-	else if (n == 0)
-	{
-		_putchar ('0');
-	}
-	else if (n <= 99)
-	{
-		_putchar (n / 10 + '0');
-		_putchar (n % 10 + '0');
-	}
-	else if (n <= 999)
-	{
-		int n2 = n / 10;
 
-		_putchar (n / 100 + '0');
-		_putchar (n2 % 10 + '0');
-		_putchar (n % 10 + '0');
-	}
-	else if (n <= 9999)
-	{
-		int n3 = n / 100;
-		int n4 = n / 10;
+	if (n / 10)
+		print_number(n / 10);
 
-		_putchar (n / 1000 + '0');
-		_putchar (n3 % 10 + '0');
-		_putchar (n4 % 10 + '0');
+	if (add == 1)
+		_putchar ('8');
+	else
 		_putchar (n % 10 + '0');
-	}
 }

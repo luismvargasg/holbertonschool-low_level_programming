da7ed11c6be2749b35dc94733791e9e30299640a
luismvargasg <1610@holbertonschool.com>
2020-02-21 06:20:58 +0000

    adding task 6

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -15,5 +15,6 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 void print_number(int n);
+char *cap_string(char *);
 
 #endif /* _HOLBERTON_H_ */

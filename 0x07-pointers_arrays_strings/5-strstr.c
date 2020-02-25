c91a1cc1293046494fd007cb4567f02c2f93089f
luismvargasg <1610@holbertonschool.com>
2020-02-25 04:57:49 +0000

    adding task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * _strstr - function that locates a substring.
+ * @haystack: given string
+ * @needle: string to find inside haystack
+ *
+ * Return: null or string
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
+	const char *a = haystack, *b = needle;
+
+	for (;;)
+	{
+		if ( !*b ) return (char *) haystack;
+
+		if ( !*a ) return (0);
+
+		if ( *a++ != *b++)
+		{
+			a = ++haystack;
+			b = needle;
+		}
+	}
+}

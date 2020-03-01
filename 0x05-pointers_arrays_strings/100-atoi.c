fb137a10dee9577d172f55b5ca1e2ac2a09ed7ed
luismvargasg <1610@holbertonschool.com>
2020-03-01 01:07:05 +0000

    adding task 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -26,10 +26,10 @@ int _atoi(char *s)
 	{
 		while (s[i] != '\0' && s[i] >= '0' && s[i] <= '9')
 		{
-			result = result * 10 + s[i] - '0';
+			result = result * 10 - (s[i] - '0');
 			i++;
 		}
 	}
-
+	sign = sign * -1;
 	return (sign * result);
 }

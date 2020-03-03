ab07f5bcafe1d3878dd6206d8c05202f9fff7e69
luismvargasg <1610@holbertonschool.com>
2020-03-03 17:28:17 +0000

    adding task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,16 +16,21 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int i = 0;
 
+	array = malloc(sizeof(char) * size);
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
 	if (size > 0)
 	{
-		array = malloc(sizeof(char) * size);
 		while (i < size)
 		{
 			array[i] = c;
 			i++;
 		}
-		return (array);
+		array[i] = '\0';
 	}
-	else
-		return (0);
+	return (array);
 }

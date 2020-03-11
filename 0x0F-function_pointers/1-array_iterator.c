89b44d262769b489b3f3612610207a495a08be02
luismvargasg <1610@holbertonschool.com>
2020-03-11 14:44:27 -0500

    adding task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -16,7 +16,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (*action)
 	{
-		while (i < size && != NULL)
+		while (i < size && array != NULL)
 			(*action)(array[i++]);
 	}
 }

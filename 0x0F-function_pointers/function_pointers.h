a0f5f798cf1cde3dcc3480d9446b480257a730e2
luismvargasg <1610@holbertonschool.com>
2020-03-11 14:40:18 -0500

    adding task 1

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef _FUNCTION_POINTERS_H_
 #define _FUNCTION_POINTERS_H_
+#include <stdlib.h>
 
 /*
  * Structs, enums and unions definitions
@@ -8,5 +9,6 @@
  */
 
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* _FUNCTION_POINTERS_H_ */

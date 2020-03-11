72cf6ace9a30eebd269a96990e12bfd9a6950415
luismvargasg <1610@holbertonschool.com>
2020-03-11 13:33:45 -0500

    adding task 0

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,12 @@
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+void print_name(char *name, void (*f)(char *));
+
+#endif /* _FUNCTION_POINTERS_H_ */

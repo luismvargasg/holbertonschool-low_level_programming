6f30832e49f3a9a610bc8a47789bba63b8137411
luismvargasg <1610@holbertonschool.com>
2020-03-13 22:08:31 +0000

    adding task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/** sum_them_all - function that returns the sum of all its parameters.
+ * @n: Given numbers.
+ *
+ * Return: Result of the sum or 0 if n == 0.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i = 0;
+	int res = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(ap, n);
+	for (; i < n; i++)
+		res += va_arg(ap, int);
+
+	va_end(ap);
+	return (res);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,14 @@
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+
+#endif /* _VARIADIC_FUNCTIONS_H_ */

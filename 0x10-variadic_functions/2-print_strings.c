73b6a8efa258bb651da201f39bc700b583bc2a27
luismvargasg <1610@holbertonschool.com>
2020-03-14 17:25:57 +0000

    fixing NULL case

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		res = va_arg(ap, char *);
 
-		if (res == NULL)
+		if (*res == '\0')
 			printf("nil");
 		else
 			printf("%s", res);

9f9d424053ac649c1141cada1d896684c07661bf
luismvargasg <1610@holbertonschool.com>
2020-03-24 20:58:19 +0000

    fixing if idx is 0 on task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
+	{
+		free(new_node);
 		return (NULL);
+	}
 
 	new_node->n = n;
 	cursor = *head;
@@ -29,6 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new_node->next = *head;
 		*head = new_node;
+		return (new_node);
 	}
 	else
 	{
@@ -37,6 +41,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 		new_node->next = cursor->next;
 		cursor->next = new_node;
+		return (new_node);
 	}
-	return (new_node);
+	return (NULL);
 }

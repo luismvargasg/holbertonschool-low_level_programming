4d58f33ca8c3f878ae7cae074e7f918942454ed2
luismvargasg <1610@holbertonschool.com>
2020-03-26 22:03:50 +0000

    adding task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,7 +18,7 @@ unsigned int binary_to_uint(const char *b)
 
 	while (b[i] != '\0')
 	{
-		if (b[i] > '0' && b[i] < '1')
+		if (b[i] >= '0' && b[i] <= '1')
 		{
 			number <<= 1;
 			number += b[i] - '0';

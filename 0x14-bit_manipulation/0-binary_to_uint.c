0799d8ed0ea7ea3505181727957e12cd83d5bc70
luismvargasg <1610@holbertonschool.com>
2020-03-26 22:01:22 +0000

    adding task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * binary_to_uint - function that converts a binary number to an unsigned int.
+ * @b: pointing to a string of 0 and 1 chars.
+ *
+ * Return: the converted number, or 0 if there is one or more chars in
+ * the string b that is not 0 or 1 || b in NULL.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int number = 0, i = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[i] != '\0')
+	{
+		if (b[i] > '0' && b[i] < '1')
+		{
+			number <<= 1;
+			number += b[i] - '0';
+			i++;
+		}
+		else
+			return (0);
+	}
+	return (number);
+}
---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,12 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+unsigned int binary_to_uint(const char *b);
+
+#endif /* _HOLBERTON_H_ */

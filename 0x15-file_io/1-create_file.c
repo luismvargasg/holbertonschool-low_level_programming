9eef86ff04f8e4547b3cf1d802935fdf69b3316d
luismvargasg <1610@holbertonschool.com>
2020-03-31 23:54:55 +0000

    adding task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  * create_file - function that creates a file.
  * @filename:is the name of the file to create.
- * @text_content: text_content is a NULL terminated string to write to the file.
+ * @text_content: text_content is a NULL terminated string to write to the file
  *
  * Return: 1 on success, -1 on failure (file can not be created, file can not
  * be written, write fails, etc) / if filename is NULL.

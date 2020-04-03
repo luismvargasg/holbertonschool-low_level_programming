73173d090f160a01e6a43cb00e3557abd40fcd4a
luismvargasg <1610@holbertonschool.com>
2020-04-03 06:11:57 +0000

    adding task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,42 +9,30 @@
 
 void times_table(void)
 {
-	int a, b, mult;
+	int a, b, mult = 0;
 
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			mult = (a * b);
-			if (mult == 0)
+			mult = a * b;
+			if (b == 0)
+				_putchar('0');
+			else if (mult < 10)
 			{
-				_putchar ('0');
-				_putchar (',');
-				_putchar (' ');
-				_putchar (' ');
-			}
-			else if (mult == 0)
-			{
-				_putchar (b);
-				_putchar (',');
-				_putchar (' ');
-				_putchar (' ');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(mult % 10 + '0');
 			}
 			else if (mult > 9)
 			{
-				_putchar (mult / 10 + '0');
-				_putchar (mult % 10 + '0');
-				_putchar (',');
-				_putchar (' ');
-			}
-			else if (mult <= 9)
-			{
-				_putchar (mult % 10 + '0');
-				_putchar (',');
-				_putchar (' ');
-				_putchar (' ');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(mult / 10 + '0');
+				_putchar(mult % 10 + '0');
 			}
 		}
-		_putchar ('\n');
+		_putchar('\n');
 	}
 }

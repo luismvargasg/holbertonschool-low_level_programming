2b047c80c4d55a1709affc2475eb1ccbb5356944
luismvargasg <1610@holbertonschool.com>
2020-05-02 22:02:03 +0000

    adding comments on task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 
 	new_node->n = n;
-	if ((*head) != NULL)
+	if ((*head) != NULL) /* check if the list is not empty != first first node */
 		(*head)->prev = new_node;
 	new_node->next = *head;
 	new_node->prev = NULL;

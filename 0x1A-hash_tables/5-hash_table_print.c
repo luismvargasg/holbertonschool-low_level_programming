c5250511e978145c5753dbdd8ab2544df5c0dcae
luismvargasg <1610@holbertonschool.com>
2020-05-29 20:32:05 +0000

    adding task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function that prints a hash table.
+ * @ht: is the hash table.
+ *
+ * Return: Nothing.
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int ix = 0, flag = 0;
+
+	if (ht)
+	{
+		printf("{");
+		for (; ix < ht->size; ix++)
+		{
+			while (ht->array[ix] != NULL)
+			{
+				if (flag == 1)
+				{
+					printf(", ");
+					flag = 0;
+				}
+				printf("%s: '%s'", ht->array[ix]->key, ht->array[ix]->value);
+				ht->array[ix] = ht->array[ix]->next;
+				flag =  1;
+			}
+		}
+		printf("}\n");
+	}
+}

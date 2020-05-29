7c7bfdbe39464aba93285f6ca0966c417b78c1cc
luismvargasg <1610@holbertonschool.com>
2020-05-29 14:50:47 +0000

    testing calloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (size == 0)
 		return (NULL);
 
-	new_ht = malloc(sizeof(hash_table_t));
+	new_ht = calloc(1, sizeof(hash_table_t));
 	if (new_ht == NULL)
 		return (NULL);
 	new_ht->size = size;

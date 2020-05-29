deb8ffb84f9b1e6534bf420d848efabc9b4d1743
luismvargasg <1610@holbertonschool.com>
2020-05-29 13:09:41 +0000

    removing libraries from header, fixing calloc on task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+#include <stdlib.h>
 
 /**
  * hash_table_create - function that creates a hash table.
@@ -18,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (new_ht == NULL)
 		return (NULL);
 	new_ht->size = size;
-	new_ht->array = calloc(sizeof(hash_node_t *) * size);
+	new_ht->array = calloc(sizeof(hash_node_t *), size);
 	if (new_ht->array == NULL)
 	{
 		free(new_ht);
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,10 +1,6 @@
 #ifndef _HASH_TABLES_H_
 #define _HASH_TABLES_H_
 
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-
 /*==========================     DATA STRUCTURES    ========================*/
 
 /**

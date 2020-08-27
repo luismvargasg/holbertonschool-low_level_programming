1fa17a2b960e9b11b38ad7fb5e23e18a185d2792
luismvargasg <1610@holbertonschool.com>
2020-08-27 23:40:37 +0000

    adding files for task 9

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -17,6 +17,8 @@ int interpolation_search(int *array, size_t size, int value)
 {
 	size_t low = 0, high = (size - 1), pos = 0;
 
+	if (array == NULL)
+		return (-1);
 	while (low <= high && value >= array[low] && value <= array[high])
 	{
 		pos = low + (((double)(high - low) / (array[high] - array[low])) *
